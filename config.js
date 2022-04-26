7ad2879f563e7c1239e5e95cb336c7e7a0f92100
supuna <92195424+supuna28@users.noreply.github.com>
2022-04-26 17:30:10 +0530

    Update config.js

--- config.js
+++ config.js
@@ -28,7 +28,7 @@ global.APIs = { // API Prefix
 global.APIKeys = { // APIKey Here
   // 'https://website': 'apikey'
   'https://melcanz.com': 'elaina',
-  'https://api.xteam.xyz': 'd37372311698ed1d',
+  'https://api.xteam.xyz': 'FuzBot1',
   'https://zahirr-web.herokuapp.com': 'zahirgans',
   'https://bsbt-api-rest.herokuapp.com': 'benniismael',
   'https://api.zeks.xyz': 'apivinz',
